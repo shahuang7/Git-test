981f892bb5c9055d658136276379be5f24d84a1d
shahuang7 <shanghui22.huang@gmail.com>
2023-10-09 16:42:18 -0500

    Save Variables, Run Diffmap Analysis

--- test1.py
+++ test1.py
@@ -31,22 +31,23 @@ class Window(QMainWindow):
         self.parameter_box("Sigma Factor:",10,210,110,30)
         self.parameter_box("nEigs:",10,250,60,30)
         self.line1 = QLineEdit("",self)
-        self.line1.setGeometry(150,50,60,30)
+        self.line1.setGeometry(160,50,60,30)
         self.line2 = QLineEdit("",self)
-        self.line2.setGeometry(150,90,60,30)
+        self.line2.setGeometry(160,90,60,30)
         self.line3 = QLineEdit("",self)
-        self.line3.setGeometry(150,130,60,30)
+        self.line3.setGeometry(160,130,60,30)
         self.line4 = QLineEdit("",self)
-        self.line4.setGeometry(150,170,60,30)
+        self.line4.setGeometry(160,170,60,30)
         self.line5 = QLineEdit("",self)
-        self.line5.setGeometry(150,210,60,30)
+        self.line5.setGeometry(160,210,60,30)
         self.line6 = QLineEdit("",self)
-        self.line6.setGeometry(150,250,60,30)
+        self.line6.setGeometry(160,250,60,30)
        
 
 
-        self.v_name,self.nN,self.sigfac = None,None,None
+        self.v_name,self.nN,self.sigfac,self.h5,self.nEigs,self.transpose = None,None,None,None,None,None
         self.save_variable()
+        self.run_button()
 
         # showing all the widgets 
         self.show() 
@@ -56,12 +57,12 @@ class Window(QMainWindow):
         fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
         if fileName:
             print('Data file ' + fileName + ' is selected')
-            self.popup_text(fileName,120,10,200,30)
+            self.popup_text(fileName,170,10,300,30)
             self.file_path = fileName
 
     def LoadFile(self):
         button_load= QPushButton("Open Data File", self)
-        button_load.setGeometry(10,10,100,30)
+        button_load.setGeometry(10,10,150,30)
         button_load.clicked.connect(self.open_dialog)
 #       
     def popup_text(self,a,x,y,w,h):
@@ -78,9 +79,16 @@ class Window(QMainWindow):
         label.show()
 
     def save_variable(self):
-        button = QPushButton("OK",self)
-        button.setGeometry(810,350,50,30) 
+        button = QPushButton("Save Parameter",self)
+        button.setGeometry(700,280,150,30) 
         button.clicked.connect(self.click_save)
+        button.clicked.connect(self.print_log)
+
+    def print_log(self):
+        text = " Data = {} \n Variable Name = {} \n h5 = {} \n Transpose = {} \n nN = {} \n Sigma Factor = {} \n nEigs = {}".format(self.file_path,self.v_name,self.h5,self.transpose,self.nN,self.sigfac,self.nEigs)
+        label = QLabel(text,self)
+        label.setGeometry(500,20,300,120)
+        label.show()
         
     def click_save(self):
 ##            global v_name,nN,sigfac
@@ -88,41 +96,80 @@ class Window(QMainWindow):
         self.h5 = bool(self.line2.text())
         self.transpose = bool(self.line3.text())
         self.nN = int(self.line4.text())
+        self.sigfac = float(self.line5.text())
+        self.nEigs = int(self.line6.text()) 
 #        print(self.v_name,self.nN,self.sigfac,self.file_path)
 
+    def run_button(self):
+        button = QPushButton("Run",self)
+        button.setGeometry(700,320,150,30) 
+        button.clicked.connect(self.click_run)
 
+    def click_run(self):
+        xyz = read_h5(self.file_path,self.v_name,self.h5,self.transpose)
+        yRow_yCol_yVal_file = 'sqDist.h5'
+        nN = self.nN
+        sigma_factor = self.sigfac
+        nEigs = self.nEigs
+        alpha = 1.0
 
+        from scipy.spatial.distance import cdist
+        sqDist = cdist(xyz,xyz,'sqeuclidean')
+        from sparsify import smallest_items_in_each_row_of_table,symmetrize
+        from misc_tools import write_h5
+        yRow,yCol,yVal = smallest_items_in_each_row_of_table(sqDist,nN)
+        yRow_symm,yCol_symm,yVal_symm = symmetrize(yRow,yCol,yVal)
+        write_h5(yRow_yCol_yVal_file,yRow_symm,'yRow')
+        write_h5(yRow_yCol_yVal_file,yCol_symm,'yCol')
+        write_h5(yRow_yCol_yVal_file,yVal_symm,'yVal')
 
-    # method for components 
-    def UiComponents(self): 
+        from ferguson import analyze as fer_ana
 
-        # creating a QLineEdit object 
-        line_edit = QLineEdit("Variable Name: ", self) 
+        sigma_opt,_ = fer_ana(yRow_yCol_yVal_file)
 
-        # setting geometry 
-        line_edit.setGeometry(180, 80, 150, 40) 
+        from diffmap import analyze as diff_ana
+        from diffmap import plot2D
+        sigma = sigma_factor*sigma_opt
+        h5_eigVec_eigVal = diff_ana(yRow_yCol_yVal_file,sigma,nEigs,alpha)
+        eigVec = read_h5(h5_eigVec_eigVal,'eigVec')
+     
 
-        # creating a label 
-    #    label = QLabel("GfG", self) 
+        for j in [1]:
+  # colored based on \psi_j
+            write_h5('colorcode.h5',eigVec[:,j]/eigVec[:,0],'colorcode')
+            figure_name = plot2D(h5_eigVec_eigVal,[1,2],s=20)
+            os.rename(figure_name,'diffmap_2D_psi_1_2_psi_{}_colored.jpg'.format(j))
 
-        # setting geometry to the label 
-    #    label.setGeometry(80, 150, 120, 60) 
+        label = QLabel("Done",self)
+        label.setGeometry(500,180,100,30)
+        label.show()
 
-        # setting word wrap property of label 
-    #    label.setWordWrap(True) 
+        plot_button = QPushButton("Display",self)
+        plot_button.setGeometry(700,360,150,30)
+#        plot_button.clicked.connect(self.plot('diffmap_2D_psi_1_2_psi_1_colored.jpg'))
+        plot_button.show()
+    
+    def plot(self,Q):
+        self.w = AnotherWindow()
+        self.w.show()
+#            w.label = QLabel(self)
+#            pixmap = QPixmap('diffmap_2D_psi_1_2_psi_1_colored.jpg')
+#            w.label.setPixmap(pixmap)
 
-        # adding action to the line edit when enter key is pressed 
-    #    line_edit.returnPressed.connect(lambda: do_action()) 
+            
+class AnotherWindow(QWidget):
+    """
+    This "window" is a QWidget. If it has no parent, it
+    will appear as a free-floating window as we want.
+    """
+    def __init__(self,a):
+        super().__init__()
+        layout = QVBoxLayout()
+        pixmap = QPixmap(a)
+        self.label.setPixmap(pixmap)
 
-        # method to do action 
-    #    def do_action(): 
-
-            # getting text from the line edit 
-     #       value = line_edit.text() 
-
-            # setting text to the label 
-      #      label.setText(value) 
-
+        layout.addWidget(self.label)
+        self.setLayout(layout)
 
 
 
@@ -133,3 +180,4 @@ App = QApplication(sys.argv)
 window = Window() 
 # start the app 
 sys.exit(App.exec()) 
+#App.exec()
